2eb2727061566c58fcd7b03c24a371b458b4cdb8
nahidnawalnayim <smnibraz5@gmail.com>
2022-09-30 15:47:34 +0600

    2nd commit

--- components/textimage.tsx
+++ components/textimage.tsx
@@ -14,28 +14,28 @@ const Textimage = () => {
       <div className="flex flex-wrap -m-2">
         <div className="p-2 w-1/2">
           <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Name</label>
+            <label className="leading-7 text-sm text-gray-200">Name</label>
         <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="p-2 w-1/2">
           <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Email</label>
+            <label className="leading-7 text-sm text-gray-200">Email</label>
         <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
         <div className="p-2 w-full">
           <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Message</label>
+            <label className="leading-7 text-sm text-gray-200">Message</label>
             <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
           </div>
         </div>
         <div className="p-2 w-full">
-          <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send Message</button>
         </div>
         <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
           <a className="text-indigo-500">smnibraz5@gmail.com</a>
-          <p className="leading-normal my-5">Foy'slake,Chattogram
+          <p className="leading-normal my-5 text-gray-300">Foy'slake,Chattogram
             <br /> 
           </p>
           <span className="inline-flex">
